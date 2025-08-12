046693e6f4e485407e2a78d10c66746bdfb365bb
Ilya Rubezhanskiy <alexswift9426@gmail.com>
2025-08-12 16:57:00 +0300

    .

---
+++ AllConsoleProjects/Projects/MethodsConsoleApp/MethodsTask1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsTask1
+{
+    internal class Program
+    {
+        static string[] SplitTextMethod(string word)
+        {
+            string[] text = new string[(word.Split(' ')).Length];
+            Array.Copy(word.Split(' '), text, word.Split(' ').Length);
+            return text;
+        }
+        static void TextOutput(string[] text)
+        {
+            foreach (string line in text)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Использование метода разделения строки в массив слов."+
+                              "\nВведите предложение, разделяя слова пробелом: ");
+            TextOutput(SplitTextMethod(Console.ReadLine()));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AllConsoleProjects/Projects/MethodsConsoleApp/MethodsTask2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsTask2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Метод разделения строки на массив слов
+        /// </summary>
+        /// <param name="word">Вводимая строка</param>
+        /// <returns></returns>
+        static string[] SplitTextMethod(string word)
+        {
+            string[] text = new string[(word.Split(' ')).Length];
+            Array.Copy(word.Split(' '), text, word.Split(' ').Length);
+            return text;
+        }
+        /// <summary>
+        /// Метод инверсии массива слов
+        /// </summary>
+        /// <param name="text">Первоначальный массив слов</param>
+        /// <returns></returns>
+        static string[] InverseTextMethod(string[] text)
+        {
+            string[] reversedText = new string[text.Length];
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                reversedText[i] = text[i];
+            }
+            return reversedText;
+        }
+        /// <summary>
+        /// Метод для проверки работы метода инверсии массива слов
+        /// </summary>
+        /// <param name="text">Инверсированный массив слов</param>
+        static void TextOutput(string[] text)
+        {
+            foreach (string line in text)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Использование метода разделения строки в массив слов и его перестановки." +
+                          "\nВведите предложение, разделяя слова пробелом: ");
+            string[] text = InverseTextMethod(SplitTextMethod(Console.ReadLine()));
+            TextOutput(text);
+            Console.ReadKey();
+        }
+    }
+}
