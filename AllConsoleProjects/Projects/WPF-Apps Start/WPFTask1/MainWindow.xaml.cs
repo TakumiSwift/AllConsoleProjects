2fa2429071e82f8a4f96960a283774e194193c85
Ilya Rubezhanskiy <alexswift9426@gmail.com>
2025-09-16 23:27:39 +0300

    .

---
+++ AllConsoleProjects/Projects/WPF-Apps Start/WPFTask1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace WPFTask1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxSentence_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textBoxSentence.Text = string.Empty;
+        }
+
+        private void btnSplitWords_Click(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<Words> words = new ObservableCollection<Words>();
+            listBoxWords.ItemsSource = words;
+            string[] lines = textBoxSentence.Text.Split(' ');
+            for (int i = 0;  i < lines.Length; i++)
+            {
+                Words w = new Words(lines[i]);
+                words.Add(w);
+            }
+            listBoxWords.ItemsSource = words;
+            textBoxSentence.Text = "Начните ввод предложения...";
+        }
+
+        private void btnMixWords_Click(object sender, RoutedEventArgs e)
+        {            
+            string[] lines = textBoxSentence.Text.Split(' ');
+            lines.Reverse();
+            string line = "";
+            for (int i = lines.Length - 1; i >= 0; i--)
+            {
+                line += $"{lines[i]} \n";
+            }
+            labelReversedSentence.Content = line;
+            textBoxSentence.Text = "Начните ввод предложения...";
+        }
+
+    }
+}
---
+++ AllConsoleProjects/Projects/WPF-Apps Start/WPFTask1/Words.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTask1
+{
+    public struct Words
+    {
+        public string Word { get; set; }
+
+        public Words(string word)
+        {
+            this.Word = word;
+        }
+    }
+}
